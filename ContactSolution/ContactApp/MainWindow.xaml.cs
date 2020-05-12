ba25d8bfdcb1fadd7b4cf819eb5b8094d93ecea1
laga5508 <lagassemark@yahoo.com>
2020-05-12 14:43:10 -0700

    Completed Homework 5

---
+++ ContactSolution/ContactApp/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using ContactApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadContacts();
+        }
+
+        private void uxFileChange_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void uxFileDelete_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void LoadContacts()
+        {
+            var contacts = App.ContactRepository.GetAll();
+
+            uxContactList.ItemsSource = contacts
+                .Select(t => ContactModel.ToModel(t))
+                .ToList();
+
+            // OR
+            //var uiContactModelList = new List<ContactModel>();
+            //foreach (var repositoryContactModel in contacts)
+            //{
+            //    This is the .Select(t => ... )
+            //    var uiContactModel = ContactModel.ToModel(repositoryContactModel);
+            //
+            //    uiContactModelList.Add(uiContactModel);
+            //}
+
+            //uxContactList.ItemsSource = uiContactModelList;
+        }
+
+        private void uxFileNew_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ContactWindow();
+
+            if (window.ShowDialog() == true)
+            {
+                var uiContactModel = window.Contact;
+
+                var repositoryContactModel = uiContactModel.ToRepositoryModel();
+
+                App.ContactRepository.Add(repositoryContactModel);
+                LoadContacts();
+
+                // OR
+                //App.ContactRepository.Add(window.Contact.ToRepositoryModel());
+            }
+        }
+
+        private void uxContactList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+          
+        }
+
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
+
+        private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            var column = (sender as GridViewColumnHeader);
+
+            string sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                uxContactList.Items.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+            {
+                newDir = ListSortDirection.Descending;
+            }
+
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            uxContactList.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool turn = true;
+        int turn_count = 0;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void uxNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+            UIElementCollection myButtons = uxGrid.Children;
+            foreach (var btn in myButtons)
+            {
+                Button b = (Button)btn;
+                b.IsEnabled = true;
+                b.Content = "";
+                uxTurn.Text = "";
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            int col = Grid.GetColumn(b);
+            int row = Grid.GetRow(b);
+               
+
+            if (turn)
+            {
+                b.Content = "X";
+              
+            }
+            else
+            {
+                b.Content = "O";
+            }
+            turn = !turn;
+            b.IsEnabled = false;
+            turn_count++;
+            CheckForWinner(col, row);
+
+        }
+
+        private void CheckForWinner(int col, int row)
+        {
+            //Wasn't sure how to use the col,row coordinates to extract the items Content so I did the below.
+            //How can the coordinates from the Tag be used to compare? 
+            bool WinnerFound = false;
+            UIElementCollection myButtons = uxGrid.Children;
+            Button btnOne = (Button) myButtons[0];
+            Button btnTwo = (Button)myButtons[1];
+            Button btnThree = (Button)myButtons[2];
+            Button btnFour = (Button)myButtons[3];
+            Button btnFive = (Button)myButtons[4];
+            Button btnSix = (Button)myButtons[5];
+            Button btnSeven = (Button)myButtons[6];
+            Button btnEight = (Button)myButtons[7];
+            Button btnNine = (Button)myButtons[8];
+
+            if (btnOne.Content == btnTwo.Content && btnTwo.Content == btnThree.Content && !btnOne.IsEnabled)
+            {
+                WinnerFound = true;
+            }
+
+            else if (btnThree.Content == btnFour.Content && btnFour.Content == btnFive.Content && !btnThree.IsEnabled)
+            {
+                WinnerFound = true;
+            }
+            else if (btnSix.Content == btnSeven.Content && btnSeven.Content == btnEight.Content && !btnSix.IsEnabled)
+            {
+                WinnerFound = true;
+            }
+
+              else if (btnOne.Content == btnFour.Content && btnFour.Content == btnSeven.Content && !btnOne.IsEnabled)
+            {
+                WinnerFound = true;
+            }
+
+            else if (btnTwo.Content == btnFive.Content && btnFive.Content == btnEight.Content && !btnTwo.IsEnabled)
+            {
+                WinnerFound = true;
+            }
+
+            else if (btnThree.Content == btnSix.Content && btnSix.Content == btnNine.Content && !btnThree.IsEnabled)
+            {
+                WinnerFound = true;
+            }
+
+            else if (btnOne.Content == btnFive.Content && btnFive.Content == btnNine.Content && !btnOne.IsEnabled)
+            {
+                WinnerFound = true;
+            }
+
+            else if (btnThree.Content == btnFive.Content && btnFive.Content == btnSeven.Content && !btnThree.IsEnabled)
+            {
+                WinnerFound = true;
+            }
+
+            if (WinnerFound)
+            {
+                disableButtons();
+                string winner = "";
+                if (turn)
+                {
+                    winner = "O";
+                }
+                else
+                {
+                    winner = "X";
+                }
+
+                uxTurn.Text = $"{winner} wins!";
+                
+            }   
+            else
+            {
+                if (turn_count==9)
+                {
+                    uxTurn.Text = "It's a draw!";
+                }
+            }
+        }
+        private void disableButtons()
+        {
+            UIElementCollection myButtons = uxGrid.Children;
+            foreach (var btn in myButtons)
+            {
+                Button b = (Button)btn;
+                b.IsEnabled = false;
+            }
+
+        }
+    }
+}
